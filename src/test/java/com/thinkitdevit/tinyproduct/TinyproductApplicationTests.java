8b06a72b3eb878d1b2acc5785cd48997f18c51f9
johndbs <83272366+johndbs@users.noreply.github.com>
2024-02-22 11:00:45 +0100

    Initial commit

---
+++ src/test/java/com/thinkitdevit/tinyproduct/TinyproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkitdevit.tinyproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
